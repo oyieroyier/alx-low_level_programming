0d106d8d981e0d49b9c3582cf5e240f40ce9b5ad
Oyier Oyier <boboyier92@gmail.com>
2022-09-14 21:47:16 +0300

    Q0
    Testing Q0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char a[] = "_putchar";
+int i = 0
+
+while(a != '\0')
+{
+    putchar(a[i]);
+    i++;
+}
+}
