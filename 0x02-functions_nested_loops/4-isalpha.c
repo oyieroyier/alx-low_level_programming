d22b3f4d32fed861b71c3e6cb8d411fd431f9eda
Bob Oyier <boboyier92@gmail.com>
2022-09-15 05:12:06 +0300

    "sda"

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_alpha - check the code.
+ *@c: the integer value it receives
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	return (1);
+
+	else
+	return (0);
+}
