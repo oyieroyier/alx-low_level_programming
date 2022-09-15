fae092a124c57863d3a5dd80abb8691f3dcded46
Bob Oyier <boboyier92@gmail.com>
2022-09-15 06:02:01 +0300

    "ad"

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @n: an integer
+ * Return: int
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+	return(n);
+	}
+	else if (n < 0)
+	{
+	reutrn (-n);
+	}
+	else
+	{
+	return (n);
+	}
+}
+
