ca668168d538e04e96c7222c470b870be7792560
Bob Oyier <boboyier92@gmail.com>
2022-09-15 05:50:33 +0300

    "k"

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,15 +8,15 @@
 
 int print_sign(int n)
 {
-	int n;
+	int i;
 
-	if (n > 0)
+	if (i > 0)
 	{
 	_putchar('+');
 	return (1);
 	}
 
-	else if (n == 0)
+	else if (i == 0)
 	{
 	_putchar('0');
 	return (0);
