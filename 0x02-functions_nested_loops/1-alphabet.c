073f485b46a22de4363b6d2b58068f06911aed8f
Bob Oyier <boboyier92@gmail.com>
2022-09-15 04:40:47 +0300

    "sd"

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,19 +8,6 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
-{
-    void print_alphabet();
-    return 0;
-
-
-/**
- * main - Entry point
- *
- *Description - Prints the alphabet
- *
- * Return: Always 0 (Success)
- */
 
 void print_alphabet(void)
 {
@@ -31,6 +18,4 @@ for (i = 'a'; i <= 'z'; i++)
 	_putchar(i);
 	}
 _putchar('\n');
-}
-
 }
