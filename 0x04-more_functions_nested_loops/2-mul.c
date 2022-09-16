baa27feb233286871d5e610b9841a15b683f95b5
Bob Oyier <boboyier92@gmail.com>
2022-09-17 02:12:29 +0300

    "2"

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * 
+ *mul - entry point
+ *
+ *@a: value taken 1
+ *@b: value taken 2
+ *
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	_putchar(a * b)
+
+	return (0);
+}
