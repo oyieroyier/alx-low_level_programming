81229fe072dca8561e99e3bea044092282db298b
Bob Oyier <boboyier92@gmail.com>
2022-09-17 00:29:27 +0300

    Q9 Done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ * Return - Always 0
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("FizzBuzz\n");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz\n");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz\n");
+		}
+		else
+		{
+			printf("%i\n",i);
+		}
+	}
+}
