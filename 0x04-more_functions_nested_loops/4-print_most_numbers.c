07b7829d8d62938b621051bff516648e884d64ed
Bob Oyier <boboyier92@gmail.com>
2022-09-17 01:37:38 +0300

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,12 @@ void print_most_numbers(void)
 {
 	int i;
 	for (i = 0; i <= 9; i++)
+	
 	{
 		if (i == 2 || i == 4)
 		{
 		continue;
-		}	
+		}
 	_putchar(i);
 	_putchar('\n');
 	}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_square - entry point
+ *
+ *@size: - value taken
+ * Return - Always 0
+ */
+
+void print_square(int size)
+{
+	int i, j;
+	
+}
