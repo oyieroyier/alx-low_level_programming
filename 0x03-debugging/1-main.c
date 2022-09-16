b43d53a13dbe2c2a162b4ad436824f9c4af568c7
Bob Oyier <boboyier92@gmail.com>
2022-09-16 05:29:03 +0300

    "Modified"

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,8 +9,16 @@ int main(void)
 {
 	int i;
 
+	printf("Infinite loop incoming :(\n");
+
 	i = 0;
-	positive_or_negative(i);
+	/*
+	*while (i < 10)
+	*{
+	*	putchar(i);
+	*}
+	*/
+printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
