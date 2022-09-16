328704dcf937f1ab89d6f7c04c2f3ab0926ecd94
Bob Oyier <boboyier92@gmail.com>
2022-09-17 01:55:09 +0300

    "5"

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *more_numbers - entry point
+ *
+ *
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
