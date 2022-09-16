b8a419c5d3f1882f61e73e6db39ba1a16f0ebfad
Bob Oyier <boboyier92@gmail.com>
2022-09-16 22:12:35 +0300

    "Q0"

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,20 @@
 #include "main.h"
 
+/*
+ *main - entry point
+ *
+ *return - Always 0
+ */
+
 int _isupper(int c)
 {
     if (c >= 'A' && c <= 'Z')
     {
-        return (1);
+	return (1);
     }
     
     else
     {
-        return (0);
+	return (0);
     }
 }
