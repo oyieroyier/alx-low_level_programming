d6bc221248826a5893cbb1f83c60e9fab9c0939d
Bob Oyier <boboyier92@gmail.com>
2022-09-20 23:45:07 +0300

    "Unaploaded files."

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,26 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j = 10;
+	
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = '0'; j <= '14'; j++)
+		while (j <= 14)
 		{
-			_putchar(j);
+			if (j > 9)
+			{
+			char ones = (j % 10);
+			char tens = (j / 10);
+			
+			j = ones + tens;
+			_putchar (j);
+			}
+
+		_putchar(j);
+		j++;
 		}
-	_putchar('\n');
+
 	}
+	_putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+void swap_int(int *a, int *b);
+{
+	
+}
