f08d1bf8a919713acbd3fa825c4280534099ee9d
Bob Oyier <boboyier92@gmail.com>
2022-09-21 00:01:43 +0300

    "Submission for task 1"

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,16 @@
 #include "main.h"
 
-void swap_int(int *a, int *b);
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
 {
-	
+	*a = 42;
+	*b = 98;
 }
